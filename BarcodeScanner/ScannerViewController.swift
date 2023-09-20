34a37132606f93afb67596173e1c7479c603af6e
Dmytro Nimchynskyi <nimchynskyi@icloud.com>
2023-09-20 18:43:04 +0200

    added camera capture session uikit view controller

---
+++ BarcodeScanner/ScannerViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ScannerViewController.swift
+//  BarcodeScanner
+//
+//  Created by Dmytro Nimchynskyi on 20/09/2023.
+//
+
+import UIKit
+import AVFoundation
+
+enum CameraError: String {
+    case invalidDeviceInput = "Something is wrong with camera. Unable to captiure the input."
+    case invalidScannedValue = "The value scanned is not valid. This app scans EAN-8, EAN-13 and QR Codes."
+}
+
+protocol ScannerViewControllerDelegate: class {
+    func didFind(barcode: String)
+    func didSurface(error: CameraError)
+}
+
+final class ScannerViewController: UIViewController {
+    let captureSession = AVCaptureSession()
+    var previewLayer: AVCaptureVideoPreviewLayer?
+    weak var scannerDelegate: ScannerViewControllerDelegate?
+    
+    init(scannerDelegate: ScannerViewControllerDelegate) {
+        super.init(nibName: nil, bundle: nil)
+        self.scannerDelegate = scannerDelegate
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        guard let previewLayer = previewLayer else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        previewLayer.frame = view.layer.bounds
+    }
+    
+    private func setupCaptureSession() {
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        if captureSession.canAddInput(videoInput) {
+            captureSession.addInput(videoInput)
+        } else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        let metaDataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metaDataOutput) {
+            captureSession.addOutput(metaDataOutput)
+            metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metaDataOutput.metadataObjectTypes = [.ean8, .ean13, .qr]
+        } else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            return
+        }
+        
+        // setting preview layer
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        // fill the view but keep aspect ration, eg. rectangle in this app
+        previewLayer!.videoGravity = .resizeAspectFill
+        
+        view.layer.addSublayer(previewLayer!)
+        
+        captureSession.startRunning()
+    }
+}
+
+extension ScannerViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        guard let object = metadataObjects.first else {
+            scannerDelegate?.didSurface(error: .invalidScannedValue)
+            return
+        }
+        
+        guard let machineReadableObject = object as? AVMetadataMachineReadableCodeObject else {
+            scannerDelegate?.didSurface(error: .invalidScannedValue)
+            return
+        }
+        
+        guard let barcode = machineReadableObject.stringValue else {
+            scannerDelegate?.didSurface(error: .invalidScannedValue)
+            return
+        }
+        
+        scannerDelegate?.didFind(barcode: barcode)
+    }
+}
